be207e8012aaf24658daaf7cf9bc4e94775448da
varunbhandia <varunbhandia@gmail.com>
2020-12-12 00:10:33 +0530

    Add | sidebar

--- lib/screens/author/invitations.dart
+++ lib/screens/author/invitations.dart
@@ -28,13 +28,53 @@ class _InvitationsListState extends State<InvitationsList> {
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         home: Scaffold(
-          appBar: AxessAppBar(),
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: Container(
+              alignment: Alignment.centerRight,
+              child: Image(image: AssetImage("images/logo.png")),
+            ),
+          ),
           backgroundColor: Palette.lightGreyBackground,
           body: Column(
             children: [
               for ( var invitation in sampleIlist ) InvitationCard(invitation)
-
             ],
+          ),
+          drawer: Drawer(
+            // Add a ListView to the drawer. This ensures the user can scroll
+            // through the options in the drawer if there isn't enough vertical
+            // space to fit everything.
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  child: Text('Drawer Header'),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                ),
+                ListTile(
+                  title: Text('Item 1'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('Item 2'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
           ),
         ));
   }
--- lib/screens/author/problem_list.dart
+++ lib/screens/author/problem_list.dart
@@ -31,10 +31,44 @@ class _ProblemListState extends State<ProblemList> {
         home: Scaffold(
           appBar: AxessAppBar(),
           backgroundColor: Palette.lightGreyBackground,
+          drawer: Drawer(
+            // Add a ListView to the drawer. This ensures the user can scroll
+            // through the options in the drawer if there isn't enough vertical
+            // space to fit everything.
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  child: Text('Drawer Header'),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                ),
+                ListTile(
+                  title: Text('Item 1'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('Item 2'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
           body: Column(
             children: [
               for ( var problem in samplePlist ) ProblemStatementWidget(problem)
-
             ],
           ),
           floatingActionButton: FloatingActionButton.extended(
