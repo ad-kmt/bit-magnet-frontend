d5e5e41df73cfa15dc3b1367e8534b0d7092e375
varunbhandia <varunbhandia@gmail.com>
2020-12-12 14:32:20 +0530

    Add | Sidebar

--- lib/screens/author/invitations.dart
+++ lib/screens/author/invitations.dart
@@ -1,5 +1,6 @@
 import 'package:bit_magnet/components/app_bar.dart';
 import 'package:bit_magnet/components/invitation_card.dart';
+import 'package:bit_magnet/components/side_bar.dart';
 import 'package:bit_magnet/models/invitation.dart';
 import 'package:bit_magnet/styles/palette.dart';
 import 'package:flutter/material.dart';
@@ -21,53 +22,14 @@ class _InvitationsListState extends State<InvitationsList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Container(
-          alignment: Alignment.centerRight,
-          child: Image(image: AssetImage("images/logo.png")),
-        ),
-      ),
+      appBar: AxessAppBar(),
       backgroundColor: Palette.lightGreyBackground,
       body: Column(
         children: [
           for ( var invitation in sampleIlist ) InvitationCard(invitation)
         ],
       ),
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('Drawer Header'),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-            ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
+      drawer: SideBar(),
     );
   }
 }
