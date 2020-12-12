cd86177c9f3381597faf33cb47b3b7aaad5e6b13
varunbhandia <varunbhandia@gmail.com>
2020-12-12 15:32:53 +0530

    Login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bit_magnet/screens/author/invitations.dart';
 import 'package:bit_magnet/screens/author/problem_list.dart';
+import 'package:bit_magnet/screens/login/login.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,6 @@ void main() {
   runApp(MaterialApp(
     title: 'aXess App',
     debugShowCheckedModeBanner: false,
-    home: InvitationsList(),
+    home: LoginScreen(),
   ));
 }
---
+++ lib/screens/login/login.dart
@@ -0,0 +1,50 @@
+import 'package:bit_magnet/screens/author/problem_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+
+const users = const {
+  'admin@axess.net': 'password',
+};
+
+class LoginScreen extends StatelessWidget {
+  Duration get loginTime => Duration(milliseconds: 2250);
+
+  Future<String> _authUser(LoginData data) {
+    print('Name: ${data.name}, Password: ${data.password}');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(data.name)) {
+        return 'Username not exists';
+      }
+      if (users[data.name] != data.password) {
+        return 'Password does not match';
+      }
+      return null;
+    });
+  }
+
+  Future<String> _recoverPassword(String name) {
+    print('Name: $name');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(name)) {
+        return 'Username not exists';
+      }
+      return null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      title: 'Login',
+      logo: 'images/logo.png',
+      onLogin: _authUser,
+      onSignup: _authUser,
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) => ProblemList(),
+        ));
+      },
+      onRecoverPassword: _recoverPassword,
+    );
+  }
+}
