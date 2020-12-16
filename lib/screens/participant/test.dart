20b4957d69117a36aff25cad632f0d8af5807e34
KMT <kumawat.aditya743@gmail.com>
2020-12-16 23:42:33 +0530

    login changes

--- lib/screens/participant/test.dart
+++ lib/screens/participant/test.dart
@@ -1,4 +1,5 @@
 import 'package:bit_magnet/components/app_bar.dart';
+import 'package:bit_magnet/components/hackathon_cover.dart';
 import 'package:bit_magnet/components/hackathon_icon_bar.dart';
 import 'package:bit_magnet/components/home_carousel.dart';
 import 'package:bit_magnet/components/problem_statement_detailed_card.dart';
@@ -16,7 +17,103 @@ class Test extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Column(
         children: [
-          HomeCarousel(),
+          HackathonCover(SampleObjects.sampleHackathon),
+          DefaultTabController(
+              length: 8, // length of tabs
+              initialIndex: 0,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Container(
+                      child: TabBar(
+                        isScrollable: true,
+                        labelColor: Colors.green,
+                        unselectedLabelColor: Colors.black,
+                        tabs: [
+                          Tab(text: 'Tab 1'),
+                          Tab(text: 'Tab 2'),
+                          Tab(text: 'Tab 3'),
+                          Tab(text: 'Tab 4'),
+                          Tab(text: 'Tab 1'),
+                          Tab(text: 'Tab 2'),
+                          Tab(text: 'Tab 3'),
+                          Tab(text: 'Tab 4'),
+                        ],
+                      ),
+                    ),
+                    Container(
+                        height: 400, //height of TabBarView
+                        decoration: BoxDecoration(
+                            border: Border(
+                                top: BorderSide(
+                                    color: Colors.grey, width: 0.5))),
+                        child: TabBarView(children: <Widget>[
+                          Container(
+                            child: Center(
+                              child: Text('Display Tab 1',
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                          ),
+                          Container(
+                            child: Center(
+                              child: Text('Display Tab 2',
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                          ),
+                          Container(
+                            child: Center(
+                              child: Text('Display Tab 3',
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                          ),
+                          Container(
+                            child: Center(
+                              child: Text('Display Tab 4',
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                          ),
+                          Container(
+                            child: Center(
+                              child: Text('Display Tab 1',
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                          ),
+                          Container(
+                            child: Center(
+                              child: Text('Display Tab 2',
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                          ),
+                          Container(
+                            child: Center(
+                              child: Text('Display Tab 3',
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                          ),
+                          Container(
+                            child: Center(
+                              child: Text('Display Tab 4',
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                          ),
+                        ]))
+                  ])),
         ],
       ),
     );
