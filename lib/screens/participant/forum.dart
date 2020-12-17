f0ba38b4de37b04e37d225191461b31a155db939
varunbhandia <varunbhandia@gmail.com>
2020-12-17 17:12:19 +0530

    forum

--- lib/screens/participant/forum.dart
+++ lib/screens/participant/forum.dart
@@ -19,16 +19,13 @@ import 'package:bit_magnet/styles/constants.dart';
 import 'package:bit_magnet/styles/palette.dart';
 import 'package:flutter/material.dart';
 
-class PHackathonDetail extends StatefulWidget {
-  final dynamic hackathon;
-
-  PHackathonDetail(this.hackathon);
+class ForumScreen extends StatefulWidget {
 
   @override
-  _PHackathonDetailState createState() => _PHackathonDetailState();
+  _ForumScreenState createState() => _ForumScreenState();
 }
 
-class _PHackathonDetailState extends State<PHackathonDetail>
+class _ForumScreenState extends State<ForumScreen>
     with SingleTickerProviderStateMixin {
   IParticipant participant;
   bool isRegistered;
@@ -42,10 +39,8 @@ class _PHackathonDetailState extends State<PHackathonDetail>
   void initState() {
     super.initState();
     participant = SampleObjects.sampleParticipant;
-    update(participant, widget.hackathon);
-
     _scrollController = ScrollController();
-    _tabController = TabController(vsync: this, length: 5);
+    _tabController = TabController(vsync: this, length: 4);
     _tabController.addListener(_smoothScrollToTop);
   }
 
@@ -57,92 +52,17 @@ class _PHackathonDetailState extends State<PHackathonDetail>
     );
   }
 
-  void update(IParticipant participant, dynamic hackathon) {
-    setState(() {
-      //LOGIC TO SEE IF PARTICIPANT'S ANY TEAM IS REGISTERED
-      //0. Get User details
-      //1. Get team details
-      //2. Get which hackathon registered. Check if matches
-      //3. Check which problem statement is/are registered
-
-      //GET THESE FIELDS FROM BACKEND
-      isRegistered = false;
-      userTeam = SampleObjects.sampleTeam;
-      userProblemStatements = [SampleObjects.sampleProblemStatement];
-    });
-  }
-
-  List<Widget> showContent() {
-    List<Widget> list = List();
-    if (isRegistered) {
-      list.add(TeamInfo(userTeam));
-      list.add(ProblemList(
-          "Your team's registered challenge(s):", userProblemStatements));
-      list.add(
-          ProblemList("Problem Statements", SampleObjects.sampleProblemList));
-    } else {
-      list.add(
-          ProblemList("Problem Statements", SampleObjects.sampleProblemList));
-    }
-    return list;
-  }
-
-  BottomAppBar showRegisterButton() {
-    if (isRegistered) {
-      return null;
-    } else {
-      return BottomAppBar(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
-          child: FlatGreenButton("Register", () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) {
-                  return Register();
-                },
-              ),
-            );
-          }),
-        ),
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
-    var editButtonCallBack = () {};
-
-    var publishButtonCallBack = () {};
-
-    var returnBottomBar = () {
-      //IF MODERATOR OR AUTHOR
-      if (false) {
-        return BottomBarTwoButtons(
-            "Edit", editButtonCallBack, "Publish", publishButtonCallBack);
-      }
-      //IF PARTICIPANT
-      else {
-        return showRegisterButton();
-      }
-    };
 
     return Scaffold(
       appBar: AxessAppBar(),
-      bottomNavigationBar: returnBottomBar(),
       backgroundColor: Palette.lightGreyBackground,
       body: NestedScrollView(
         controller: ScrollController(),
         headerSliverBuilder: (context, value) {
           return [
-            SliverToBoxAdapter(
-              child: Column(
-                children: [
-                  HackathonCover(widget.hackathon),
-                  HackathonIconBar(widget.hackathon),
-                ],
-              ),
-            ),
             SliverToBoxAdapter(
               child: Container(
                 child: TabBar(
@@ -156,7 +76,6 @@ class _PHackathonDetailState extends State<PHackathonDetail>
                     Tab(text: 'Problem Statements'),
                     Tab(text: 'Teams'),
                     Tab(text: 'Interested Lobby'),
-                    Tab(text: 'Submissions'),
                   ],
                 ),
               ),
@@ -173,215 +92,27 @@ class _PHackathonDetailState extends State<PHackathonDetail>
               SingleChildScrollView(
                 controller: ScrollController(),
                 child: Container(
-                  child: TeamInfo(userTeam),
+                  child: Icon(Icons.directions_car),
                 ),
               ),
               SingleChildScrollView(
                 controller: ScrollController(),
                 child: Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: List.generate(
-                        SampleObjects.sampleProblemList.length, (index) {
-                      return ProblemStatementCard(
-                          SampleObjects.sampleProblemList[index]);
-                    }),
-                  ),
+                  child: Icon(Icons.directions_car),
+
                 ),
               ),
               SingleChildScrollView(
                 controller: ScrollController(),
-                child: Container(
-                    child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: TeamCard(SampleObjects.sampleTeam),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: TeamCard(SampleObjects.sampleTeam),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: TeamCard(SampleObjects.sampleTeam),
-                    ),
-                  ],
-                )),
-              ),
-              SingleChildScrollView(
-                controller: ScrollController(),
-                child: Container(
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: LobbyCard(),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: LobbyCard(),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: LobbyCard(),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                child: Center(
-                  child: Text('Display Tab 1',
-                      style:
-                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-                ),
-              ),
-            ],
-          ),
-        ),
+                child: Icon(Icons.directions_car),
       ),
-    );
-  }
-}
-
-class TeamInfo extends StatelessWidget {
-  final ITeam team;
-  TeamInfo(this.team);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          child: Text(
-            "You're registered for this hackathon",
-            style: kBlackSubTitle,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
-          child: Row(
-            children: <Widget>[
-              Icon(
-                Icons.stars,
-                color: Palette.darkBlue,
-                size: 24,
+              SingleChildScrollView(
+                controller: ScrollController(),
+                child: Icon(Icons.directions_car),
               ),
-              SizedBox(
-                // here put the desired space between the icon and the text
-                width: 10,
-              ),
-              Row(
-                children: [
-                  Text(
-                    "Your team: ",
-                    style: kDarkBlueSubHeading,
-                  ),
-                  Text(
-                    team.name,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ],
-              )
             ],
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
-          child: Row(
-            children: <Widget>[
-              Icon(
-                Icons.people,
-                color: Palette.darkBlue,
-                size: 24,
-              ),
-              SizedBox(
-                // here put the desired space between the icon and the text
-                width: 10,
-              ),
-              Text(
-                "Your teammates:",
-                style: kDarkBlueSubHeading,
-              )
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: List.generate(team.participants.length, (index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 6),
-                child: TeamMember(team.participants[index].fullName()),
-              );
-            }),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class TeamMember extends StatelessWidget {
-  final String name;
-
-  TeamMember(this.name);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        CircleAvatar(
-          radius: 24,
-          backgroundColor: Palette.lightGreyContainer,
-          child: Icon(
-            Icons.person,
-            size: 28,
-            color: Colors.white,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Text(
-            name,
-            style: TextStyle(fontSize: 18),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ProblemList extends StatelessWidget {
-  final List<IProblemStatement> problems;
-  final String heading;
-
-  ProblemList(this.heading, this.problems);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            heading,
-            style: kBlackSubTitle,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: List.generate(problems.length, (index) {
-              return ProblemStatementCard(problems[index]);
-            }),
-          ),
-        ],
       ),
     );
   }
